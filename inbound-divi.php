b7185ec80ddd68037745074b3a0e083935c3e548
GITUSER <hudson@inboundnow.com>
2017-03-30 17:36:04 -0500

    working v1

--- inbound-divi.php
+++ inbound-divi.php
@@ -33,13 +33,13 @@ if (!class_exists('Inbound_DIVI')) {
         public static function define_constants() {
             define('INBOUND_DIVI_CURRENT_VERSION', '1.0.1');
             define('INBOUND_DIVI_LABEL', __('Divi Integration', 'inbound-pro'));
-            define('INBOUND_DIVI_SLUG', 'inbound-divi' );
+            define('INBOUND_DIVI_SLUG', 'inbound-divi');
             define('INBOUND_DIVI_FILE', __FILE__);
             define('INBOUND_DIVI_REMOTE_ITEM_NAME', 'inbound-divi');
             define('INBOUND_DIVI_PATH', realpath(dirname(__FILE__)) . '/');
             $upload_dir = wp_upload_dir();
-            $url = ( !strstr( INBOUND_DIVI_PATH , 'plugins' )) ? $upload_dir['baseurl'] . '/inbound-pro/extensions/' .plugin_basename( basename(__DIR__) ) .'/' : WP_PLUGIN_URL.'/'.plugin_basename( dirname(__FILE__) ).'/' ;
-            define('INBOUND_DIVI_URLPATH', $url );
+            $url = (!strstr(INBOUND_DIVI_PATH, 'plugins')) ? $upload_dir['baseurl'] . '/inbound-pro/extensions/' . plugin_basename(basename(__DIR__)) . '/' : WP_PLUGIN_URL . '/' . plugin_basename(dirname(__FILE__)) . '/';
+            define('INBOUND_DIVI_URLPATH', $url);
         }
 
         /**
@@ -50,24 +50,23 @@ if (!class_exists('Inbound_DIVI')) {
             /* WP-Admin Only */
             if (is_admin()) {
                 /* support post types */
-                add_filter('et_builder_post_types', array( __CLASS__ , 'support_post_types' ) );
-                add_filter('et_fb_post_types', array( __CLASS__ , 'support_post_types' ) ); 
-                
+                add_filter('et_builder_post_types', array(__CLASS__, 'support_post_types'));
+                add_filter('et_fb_post_types', array(__CLASS__, 'support_post_types'));
+
                 /* add divi settings metabox */
-                add_action('add_meta_boxes', array( __CLASS__ , 'add_meta_boxes') );
-                
+                add_action('add_meta_boxes', array(__CLASS__, 'add_meta_boxes'));
+
                 /* adds supporting js */
-                add_action('admin_head', array( __CLASS__ , 'add_admin_js' ) );
+                add_action('admin_head', array(__CLASS__, 'add_admin_js'));
             }
-
-
+            
         }
 
         /**
          *  Adds custom post type support for 'landing-page' to Divi theme.
          */
-        public static function support_post_types($post_types){
-            $post_types[] = 'landing-page';            
+        public static function support_post_types($post_types) {
+            $post_types[] = 'landing-page';
             return $post_types;
         }
 
@@ -76,26 +75,52 @@ if (!class_exists('Inbound_DIVI')) {
          */
         public static function add_meta_boxes() {
             global $post;
-                       
-            add_meta_box('et_settings_meta_box', __('Divi Settings', 'Divi'), 'et_single_settings_meta_box', 'landing-page' , 'side', 'high');
-            
-        }
-        
-        public static function add_admin_js(){
-            
-            $s = get_current_screen();
-            if ( !isset($s) || empty($s->post_type) || $s->post_type != 'landing-page' ) {
+
+            $template = Landing_Pages_Variations::get_current_template($post->ID);
+
+            if ($template != 'default') {
                 return;
             }
-            ?>
-            <script>
-                jQuery(function ($) {
-                    jQuery('#et_pb_layout').insertAfter(jQuery('#et_pb_main_editor_wrap'));
-                });
-            </script>
-            <?php
-            
-}
+
+            add_meta_box('et_settings_meta_box', __('Divi Settings', 'Divi'), 'et_single_settings_meta_box', 'landing-page', 'side', 'high');
+
+        }
+
+        public static function add_admin_js() {
+            global $post;
+
+
+            $s = get_current_screen();
+            if (!isset($s) || empty($s->post_type) || $s->post_type != 'landing-page') {
+                return;
+            }
+
+            if (!isset($post->ID)) {
+                return;
+            }
+
+            $template = Landing_Pages_Variations::get_current_template($post->ID);
+
+            if ($template == 'default') {
+
+                ?>
+                <script>
+                    jQuery(function ($) {
+                        jQuery('#et_pb_layout').insertAfter(jQuery('#et_pb_main_editor_wrap'));
+                    });
+                </script>
+                <?php
+            } else {
+                ?>
+                <script>
+                    jQuery(function ($) {
+                        jQuery('.et_pb_toggle_builder_wrapper').hide();
+                    });
+                </script>
+                <?php
+            }
+
+        }
     }
 
 
